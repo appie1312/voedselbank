3544c54fae680876ef29ee68630112aca33ec7c3
simon-2006 <341037@student.mboutrecht.nl>
2026-04-09 10:47:28 +0200

    voorraad controller gemaakt

--- app/Http/Controllers/VoorraadController.php
+++ app/Http/Controllers/VoorraadController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Voorraad;
 
+/**
+ * Controller voor het tonen van het voorraadoverzicht.
+ */
 class VoorraadController extends Controller
 {
-    //
+    /**
+     * Toon een overzicht van alle voorraadregels.
+     *
+     * Acceptatiecriteria:
+     * - Als er voorraad is: toon lijst met producten en hun voorraadstatus
+     * - Als er geen voorraad is: toon melding
+     */
+    public function index()
+    {
+        // Haal voorraad op inclusief product en categorie
+        $voorraadItems = Voorraad::with(['product.categorie'])
+            ->orderBy('id', 'asc')
+            ->get();
+
+        // Geef de data door aan de view
+        return view('voorraad.index', compact('voorraadItems'));
+    }
 }
